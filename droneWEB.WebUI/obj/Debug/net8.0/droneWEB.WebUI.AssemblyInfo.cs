8f6a1abcc9e453cf8526bdeefa6db9305232ce0b
CerenBoyaci <191340112+CerenBoyaci@users.noreply.github.com>
2025-10-02 16:49:29 +0300

    sayfalara css eklemeleri yapıldı

--- droneWEB.WebUI/obj/Debug/net8.0/droneWEB.WebUI.AssemblyInfo.cs
+++ droneWEB.WebUI/obj/Debug/net8.0/droneWEB.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("droneWEB.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9e789eabfa20a059c1e73cd96ef2be8dbe81da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414d980ebce0171992b056a149020a593bb9c52d")]
 [assembly: System.Reflection.AssemblyProductAttribute("droneWEB.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("droneWEB.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
